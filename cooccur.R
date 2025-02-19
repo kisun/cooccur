d2f6bdc91356b1f14f5522948f137a44012e4dd3
kisun <kisun.pokharel@luke.fi>
2025-02-19 08:59:07 +0200

    updated

--- cooccur.R
+++ cooccur.R
@@ -1,181 +1,319 @@
+# Set encoding and library paths
 options(encoding = "UTF-8")
 .libPaths(c("/projappl/project_2005433/project_rpackages_4.2.1", .libPaths()))
 libpath <- .libPaths()[1]
 
+# Load necessary libraries and check versions
 library(cooccur); packageVersion("cooccur")
 library(visNetwork); packageVersion("visNetwork")
 library(dplyr); packageVersion("dplyr")
 library(tidyr); packageVersion("tidyr")
+library(VennDiagram)
+library(grid)
 
+# Set working directory
 setwd("/scratch/project_2005433/KisunPokharel/projects/meilahti17/")
 
-#read in sample information table (st = sample table)
-st<-read.table("meilahti17_cooccurrence/sample_table.txt", header=TRUE, sep = "\t")
+# Read sample information table
+st <- read.table("meilahti17_cooccurrence/sample_table.txt", header = TRUE, sep = "\t")
+st <- data.frame(st)  # Convert to data frame for consistency
 dim(st)
-st
-st <- data.frame(st)
-#read in the output file that has all occurrence data
-output_data<-read.table("meilahti17_cooccurrence/data_table.txt", header = TRUE, sep = "\t")
+print(st)
+
+# Read occurrence data
+output_data <- read.table("meilahti17_cooccurrence/data_table.txt", header = TRUE, sep = "\t")
 dim(output_data)
-
 head(output_data)
-#create a subset of taxonomy information
-tax<-output_data %>% select(1:6)
+
+# Extract taxonomy information
+tax <- output_data %>% select(1:6)
 head(tax)
-phylum <- tax[,1]
+
+# Define taxonomic ranks
+phylum <- tax[, 1]
 class <- tax[, 2]
-order<-tax[,3]
-family<-tax[,4]
-genus<-tax[,5]
-species<-tax[,6]
+order <- tax[, 3]
+family <- tax[, 4]
+genus <- tax[, 5]
+species <- tax[, 6]
 
-#create a subset of all samples and data (dt = data table)
-dt<-output_data %>% select(-c(1:6))
+# Extract sample data without taxonomy
+dt <- output_data %>% select(-c(1:6))
+head(dt)
+dim(dt)
 
+# Function to merge duplicate row names by summing values into one row
+merge_duplicates <- function(mat) {
+  row_names <- rownames(mat)
+  unique_names <- unique(row_names)
+  new_mat <- matrix(nrow = 0, ncol = ncol(mat))
+  colnames(new_mat) <- colnames(mat)
+  
+  for(name in unique_names) {
+    indices <- which(row_names == name)
+    if(length(indices) > 1) {
+      # Sum the rows for duplicates into one row
+      merged_row <- colSums(mat[indices, ])
+      new_mat <- rbind(new_mat, ifelse(merged_row > 0, 1, 0))
+      rownames(new_mat)[nrow(new_mat)] <- name
+    } else {
+      # If no duplicates, just add the row
+      new_mat <- rbind(new_mat, ifelse(mat[indices, ] > 0, 1, 0))
+      rownames(new_mat)[nrow(new_mat)] <- name
+    }
+  }
+  return(new_mat)
+}
 
-########## WEST ##############
-
-##  Urban west (uw)
-#create subset of sample information to get all the urban part of the cities that belong to "west" block of countries.
-uw <- (st %>% filter(urban_suburban == "Urban", west_east == "West"))
-uw
-uw_id <- uw[,1]
-uw_id
+# Analysis by Family. If you want to use different level of taxa just replace family with what level you want. Eg., genus, phylum, etc.
+## Urban West
+uw <- st %>% filter(urban_suburban == "Urban", west_east == "West")
+uw_id <- uw[, 1]
 uw_dt <- dt %>% select(all_of(uw_id))
 head(uw_dt)
-dim(uw_dt)
 
-##########GENUS###########
+# Prepare binary matrix
+uw_dt_bin <- apply(uw_dt, 2, function(x) ifelse(x > 0, 1, x))
+rownames(uw_dt_bin) <- family
 
-#prepare binary matrix
-uw_dt_bin<-apply(uw_dt, 2, function(x) ifelse(x >0, 1, x))
-rownames(uw_dt_bin)<-genus
-head(uw_dt_bin)
+# Clean up matrix
+uw_dt_bin <- uw_dt_bin[!is.na(rownames(uw_dt_bin)), ]
 dim(uw_dt_bin)
 
-# Run cooccurrence analysis
-c_uw <- print(cooccur(uw_dt_bin, spp_names = TRUE))
+# Merge duplicates
+uw_dt_bin <- merge_duplicates(uw_dt_bin)
+dim(uw_dt_bin)
 
-write.csv(c_uw, file = "cooccur_uw_genus.csv")
-head(c_uw)
+# Filter out zero-sum rows
+uw_dt_bin1 <- uw_dt_bin[rowSums(uw_dt_bin) != 0, ]
+dim(uw_dt_bin1)
+head(uw_dt_bin1)
 
-# Prepare nodes and edges for urban west visualization
-n_uw <- data.frame(id = 1:nrow(uw_dt), label = rownames(uw_dt_bin), shadow = TRUE)
-e_uw <- data.frame(from = c_uw$sp1,
-                           to = c_uw$sp2,
-                           color = ifelse(c_uw$p_lt <= 0.05, "#B0B2C1", "#3C3F51"),
-                           dashes = ifelse(c_uw$p_lt <= 0.05, TRUE, FALSE))
+# Co-occurrence analysis
+c_uw <- cooccur(uw_dt_bin1, spp_names = TRUE)
+summary(c_uw)
+pc_uw <- print(c_uw)
+nrow(pc_uw)
 
+# Save results
+write.csv(pc_uw, file = "cooccur_uw_family.csv")
+
+# Prepare visualization data
+n_uw <- data.frame(id = 1:nrow(uw_dt_bin1), label = rownames(uw_dt_bin1), shadow = TRUE)
+dim(n_uw)
+e_uw <- data.frame(
+  from = pc_uw$sp1,
+  to = pc_uw$sp2,
+  color = ifelse(pc_uw$p_lt <= 0.05, "#B0B2C1", "#3C3F51"),
+  dashes = ifelse(pc_uw$p_lt <= 0.05, TRUE, FALSE)
+)
+
+# Visualize network
 uw_nicely <- visNetwork(nodes = n_uw, edges = e_uw) %>% visIgraphLayout(layout = "layout_nicely")
-visSave(uw_nicely, file = "uw_nicely.html")
+visSave(uw_nicely, file = "uw_family_nicely.html")
 uw_kk <- visNetwork(nodes = n_uw, edges = e_uw) %>% visIgraphLayout(layout = "layout_with_kk")
-visSave(uw_kk, file = "uw_kk.html")
-uw_fr <- visNetwork(nodes = n_uw, edges = e_uw) %>% visIgraphLayout(layout = "layout_with_fr")
-visSave(uw_fr, file = "uw_fr.html")
+visSave(uw_kk, file = "uw_family_kk.html")
 
-
-##  suburban west (sw)
-#create subset of sample information to get all the suburban part of the cities that belong to "west" block of countries.
+## Suburban West
 sw <- st %>% filter(urban_suburban == "Suburban", west_east == "West")
-#get all samples in main data table that belong to sw
-sw
-sw_id <- sw[,1]
-sw_id
+sw_id <- sw[, 1]
 sw_dt <- dt %>% select(all_of(sw_id))
 head(sw_dt)
-dim(sw_dt)
 
+# Prepare binary matrix
+sw_dt_bin <- apply(sw_dt, 2, function(x) ifelse(x > 0, 1, x))
+rownames(sw_dt_bin) <- family
 
-#prepare binary matrix
-sw_dt_bin<-apply(sw_dt, 2, function(x) ifelse(x >0, 1, x))
-rownames(sw_dt_bin)<-genus
-head(sw_dt_bin)
-# Run cooccurrence analysis
-c_sw <- print(cooccur(sw_dt_bin, spp_names = TRUE))
+# Clean up matrix
+sw_dt_bin <- sw_dt_bin[!is.na(rownames(sw_dt_bin)), ]
+dim(sw_dt_bin)
 
-write.csv(c_sw, file = "cooccur_sw.csv")
-head(c_sw)
+# Merge duplicates
+sw_dt_bin <- merge_duplicates(sw_dt_bin)
+dim(sw_dt_bin)
 
-# Prepare nodes and edges for suburban west visualization
-n_sw <- data.frame(id = 1:nrow(sw_dt), label = rownames(sw_dt_bin), shadow = TRUE)
-e_sw <- data.frame(from = c_sw$sp1,
-                              to = c_sw$sp2,
-                              color = ifelse(c_sw$p_lt <= 0.05, "#B0B2C1", "#3C3F51"),
-                              dashes = ifelse(c_sw$p_lt <= 0.05, TRUE, FALSE))
+# Filter out zero-sum rows
+sw_dt_bin1 <- sw_dt_bin[rowSums(sw_dt_bin) != 0, ]
+dim(sw_dt_bin1)
+head(sw_dt_bin1)
 
-visNetwork(nodes = n_sw, edges = e_sw) %>% visIgraphLayout(layout = "layout_nicely")
-visNetwork(nodes = n_sw, edges = e_sw) %>% visIgraphLayout(layout = "layout_with_kk")
-visNetwork(nodes = n_sw, edges = e_sw) %>% visIgraphLayout(layout = "layout_with_fr")
+# Co-occurrence analysis
+c_sw <- cooccur(sw_dt_bin1, spp_names = TRUE)
+summary(c_sw)
+pc_sw <- print(c_sw)
+nrow(pc_sw)
 
+# Save results
+write.csv(pc_sw, file = "cooccur_sw_family.csv")
 
-########## EAST ##############
+# Prepare visualization data
+n_sw <- data.frame(id = 1:nrow(sw_dt_bin1), label = rownames(sw_dt_bin1), shadow = TRUE)
+dim(n_sw)
+e_sw <- data.frame(
+  from = pc_sw$sp1,
+  to = pc_sw$sp2,
+  color = ifelse(pc_sw$p_lt <= 0.05, "#B0B2C1", "#3C3F51"),
+  dashes = ifelse(pc_sw$p_lt <= 0.05, TRUE, FALSE)
+)
 
-##  Urban East
-# Create a subset of sample information to get all the urban parts of the cities that belong to the "east" block of countries.
+# Visualize network
+sw_nicely <- visNetwork(nodes = n_sw, edges = e_sw) %>% visIgraphLayout(layout = "layout_nicely")
+visSave(sw_nicely, file = "sw_family_nicely.html")
+sw_kk <- visNetwork(nodes = n_sw, edges = e_sw) %>% visIgraphLayout(layout = "layout_with_kk")
+visSave(sw_kk, file = "sw_family_kk.html")
+
+## Urban East
 ue <- st %>% filter(urban_suburban == "Urban", west_east == "East")
-head(ue)
-ue_id <- ue[,1]
-ue_id
+ue_id <- ue[, 1]
 ue_dt <- dt %>% select(all_of(ue_id))
 head(ue_dt)
-dim(ue_dt)
 
 # Prepare binary matrix
 ue_dt_bin <- apply(ue_dt, 2, function(x) ifelse(x > 0, 1, x))
-rownames(ue_dt_bin) <- genus
-head(ue_dt_bin)
+rownames(ue_dt_bin) <- family
+
+# Clean up matrix
+ue_dt_bin <- ue_dt_bin[!is.na(rownames(ue_dt_bin)), ]
 dim(ue_dt_bin)
 
-# Run co-occurrence analysis
-c_ue <- print(cooccur(ue_dt_bin, spp_names = TRUE))
+# Merge duplicates
+ue_dt_bin <- merge_duplicates(ue_dt_bin)
+dim(ue_dt_bin)
 
-# Save co-occurrence results to CSV
-write.csv(c_ue, file = "cooccur_ue.csv", row.names = FALSE)
-head(c_ue)
+# Filter out zero-sum rows
+ue_dt_bin1 <- ue_dt_bin[rowSums(ue_dt_bin) != 0, ]
+dim(ue_dt_bin1)
+head(ue_dt_bin1)
 
-# Prepare nodes and edges for urban east visualization
-n_ue <- data.frame(id = 1:nrow(ue_dt), label = rownames(ue_dt_bin), shadow = TRUE)
-e_ue <- data.frame(from = c_ue$sp1,
-                           to = c_ue$sp2,
-                           color = ifelse(c_ue$p_lt <= 0.05, "#B0B2C1", "#3C3F51"),
-                           dashes = ifelse(c_ue$p_lt <= 0.05, TRUE, FALSE))
+# Co-occurrence analysis
+c_ue <- cooccur(ue_dt_bin1, spp_names = TRUE)
+summary(c_ue)
+pc_ue <- print(c_ue)
+nrow(pc_ue)
 
-# Visualize Urban East Network
-visNetwork(nodes = n_ue, edges = e_ue) %>% visIgraphLayout(layout = "layout_nicely")
-visNetwork(nodes = n_ue, edges = e_ue) %>% visIgraphLayout(layout = "layout_with_kk")
-visNetwork(nodes = n_ue, edges = e_ue) %>% visIgraphLayout(layout = "layout_with_fr")
+# Save results
+write.csv(pc_ue, file = "cooccur_ue_family.csv")
 
-##  Suburban East
-# Create a subset of sample information to get all the suburban parts of the cities that belong to the "east" block of countries.
+# Prepare visualization data
+n_ue <- data.frame(id = 1:nrow(ue_dt_bin1), label = rownames(ue_dt_bin1), shadow = TRUE)
+dim(n_ue)
+e_ue <- data.frame(
+  from = pc_ue$sp1,
+  to = pc_ue$sp2,
+  color = ifelse(pc_ue$p_lt <= 0.05, "#B0B2C1", "#3C3F51"),
+  dashes = ifelse(pc_ue$p_lt <= 0.05, TRUE, FALSE)
+)
+
+# Visualize network
+ue_nicely <- visNetwork(nodes = n_ue, edges = e_ue) %>% visIgraphLayout(layout = "layout_nicely")
+visSave(ue_nicely, file = "ue_family_nicely.html")
+ue_kk <- visNetwork(nodes = n_ue, edges = e_ue) %>% visIgraphLayout(layout = "layout_with_kk")
+visSave(ue_kk, file = "ue_family_kk.html")
+
+## Suburban East
 se <- st %>% filter(urban_suburban == "Suburban", west_east == "East")
-head(se)
-se_id <- se[,1]
-se_id
+se_id <- se[, 1]
 se_dt <- dt %>% select(all_of(se_id))
 head(se_dt)
-dim(se_dt)
 
 # Prepare binary matrix
 se_dt_bin <- apply(se_dt, 2, function(x) ifelse(x > 0, 1, x))
-rownames(se_dt_bin) <- genus
-head(se_dt_bin)
+rownames(se_dt_bin) <- family
 
-# Run co-occurrence analysis for suburban east
-c_se <- print(cooccur(se_dt_bin, spp_names = TRUE))
+# Clean up matrix
+se_dt_bin <- se_dt_bin[!is.na(rownames(se_dt_bin)), ]
+dim(se_dt_bin)
 
-# Save co-occurrence results to CSV
-write.csv(c_se, file = "cooccur_se.csv", row.names = FALSE)
-head(c_se)
+# Merge duplicates
+se_dt_bin <- merge_duplicates(se_dt_bin)
+dim(se_dt_bin)
 
-# Prepare nodes and edges for suburban east visualization
-n_se <- data.frame(id = 1:nrow(se_dt), label = rownames(se_dt_bin), shadow = TRUE)
-e_se <- data.frame(from = c_se$sp1,
-                   to = c_se$sp2,
-                   color = ifelse(c_se$p_lt <= 0.05, "#B0B2C1", "#3C3F51"),
-                   dashes = ifelse(c_se$p_lt <= 0.05, TRUE, FALSE))
+# Filter out zero-sum rows
+se_dt_bin1 <- se_dt_bin[rowSums(se_dt_bin) != 0, ]
+dim(se_dt_bin1)
+head(se_dt_bin1)
 
-# Visualize Suburban East Network
-visNetwork(nodes = n_se, edges = e_se) %>% visIgraphLayout(layout = "layout_nicely")
-visNetwork(nodes = n_se, edges = e_se) %>% visIgraphLayout(layout = "layout_with_kk")
-visNetwork(nodes = n_se, edges = e_se) %>% visIgraphLayout(layout = "layout_with_fr")
+# Co-occurrence analysis
+c_se <- cooccur(se_dt_bin1, spp_names = TRUE)
+summary(c_se)
+pc_se <- print(c_se)
+nrow(pc_se)
 
+# Save results
+write.csv(pc_se, file = "cooccur_se_family.csv")
+
+# Prepare visualization data
+n_se <- data.frame(id = 1:nrow(se_dt_bin1), label = rownames(se_dt_bin1), shadow = TRUE)
+dim(n_se)
+e_se <- data.frame(
+  from = pc_se$sp1,
+  to = pc_se$sp2,
+  color = ifelse(pc_se$p_lt <= 0.05, "#B0B2C1", "#3C3F51"),
+  dashes = ifelse(pc_se$p_lt <= 0.05, TRUE, FALSE)
+)
+
+# Visualize network
+se_nicely <- visNetwork(nodes = n_se, edges = e_se) %>% visIgraphLayout(layout = "layout_nicely")
+visSave(se_nicely, file = "se_family_nicely.html")
+se_kk <- visNetwork(nodes = n_se, edges = e_se) %>% visIgraphLayout(layout = "layout_with_kk")
+visSave(se_kk, file = "se_family_kk.html")
+
+# Unique Families Analysis
+# Extract row names for each subset
+rnse <- rownames(se_dt_bin1)
+rnsw <- rownames(sw_dt_bin1)
+rnue <- rownames(ue_dt_bin1)
+rnuw <- rownames(uw_dt_bin1)
+
+# Find unique items
+unique_se <- setdiff(rnse, c(rnsw, rnue, rnuw))
+unique_sw <- setdiff(rnsw, c(rnse, rnue, rnuw))
+unique_ue <- setdiff(rnue, c(rnse, rnsw, rnuw))
+unique_uw <- setdiff(rnuw, c(rnse, rnsw, rnue))
+
+# Format data for CSV
+df_length <- max(length(unique_se), length(unique_sw), length(unique_ue), length(unique_uw))
+data <- data.frame(
+  "Suburban East" = c(unique_se, rep("", df_length - length(unique_se))),
+  "Suburban West" = c(unique_sw, rep("", df_length - length(unique_sw))),
+  "Urban East" = c(unique_ue, rep("", df_length - length(unique_ue))),
+  "Urban West" = c(unique_uw, rep("", df_length - length(unique_uw)))
+)
+
+# Save to CSV
+write.csv(data, "unique_items_family.csv", row.names = FALSE, na = "")
+
+# Venn Diagram for Family Overlaps
+sets <- list(
+  "Suburban East" = rnse,
+  "Suburban West" = rnsw,
+  "Urban East" = rnue,
+  "Urban West" = rnuw
+)
+
+# Create Venn Diagram
+pdf("Venn_4way_family.pdf")
+venn_plot <- venn.diagram(
+  x = sets,
+  filename = NULL,
+  output = FALSE,
+  
+  # Visual settings
+  lwd = 0,  # No border lines
+  lty = "blank",
+  fill = c("dodgerblue", "goldenrod1", "darkorange1", "seagreen3"),
+  
+  cex = 0.8,
+  fontface = "bold",
+  fontfamily = "sans",
+  
+  cat.cex = 1,
+  cat.fontface = "bold",
+  cat.default.pos = "outer",
+  cat.fontfamily = "sans",
+  rotation.degree = 0,
+  margin = 0.2
+)
+
+grid.draw(venn_plot)  # Render the Venn diagram
+dev.off()  # Close the PDF file
